ae9cb4248b43cc115bff53e514e269f05a413a4d
thug_ken <jryu1521@gmail.com>
2024-07-20 20:26:53 +0900

    Vue Router

---
+++ src/layouts/TheView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="container py-4">
+      <RouterView></RouterView>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
